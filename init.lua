72139f0a0eea454ad7c71b1d02df76088de83a78
Nigel Nindo <nigelnindodev@gmail.com>
2024-03-01 16:51:13 +0300

    Disable Kickstart Nvim Typescript Linting Configure Conform Linting

--- init.lua
+++ init.lua
@@ -590,6 +590,17 @@ local servers = {
   },
 }
 
+-- Setup conform
+require('conform').setup({
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true
+  },
+  formatters_by_ft = {
+    typescript = { { 'prettierd', 'prettier' } }
+  }
+})
+
 -- Setup neovim lua configuration
 require('neodev').setup()
 
