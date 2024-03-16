8a63228eac0972aab2c7c811e62d2ff0a4c7bfcb
Nigel Nindo <nigelnindodev@gmail.com>
2024-03-16 19:20:05 +0300

    Fix linting

--- lua/custom/plugins/dadbod.lua
+++ lua/custom/plugins/dadbod.lua
@@ -1,8 +1,15 @@
 return {
   'kristijanhusak/vim-dadbod-ui',
   dependencies = {
-    { 'tpope/vim-dadbod',                     lazy = true },
-    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+    {
+      'tpope/vim-dadbod',
+      lazy = true
+    },
+    {
+      'kristijanhusak/vim-dadbod-completion',
+      ft = { 'sql', 'mysql', 'plsql' },
+      lazy = true
+    },
   },
   cmd = {
     'DBUI',
